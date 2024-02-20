02f21ebd51e55a37e1c5f5cf9d67e0fc8a7f78ea
AlAdler <AlAdler@users.noreply.github.com>
2024-02-20 08:35:18 +0000

    Fix styling

--- routes/routes.php
+++ routes/routes.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Storage;
 foreach (config('local-temporary-url.disk') as $disk) {
     Route::get("$disk/temp/{path}", function (string $path) use ($disk) {
         $file = Storage::disk($disk)->get($path);
+
         return response($file, 200, ['Content-Type' => Storage::mimeType($path)]);
     })
         ->where('path', '.*')
