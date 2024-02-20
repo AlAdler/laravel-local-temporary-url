292185f66f7ce6e16f777727148cab13e5c5b945
Alain Adler <al@aladler.com>
2024-02-20 00:31:43 -0800

    respond differently according to file mime type

--- routes/routes.php
+++ routes/routes.php
@@ -5,7 +5,8 @@ use Illuminate\Support\Facades\Storage;
 
 foreach (config('local-temporary-url.disk') as $disk) {
     Route::get("$disk/temp/{path}", function (string $path) use ($disk) {
-        return Storage::disk($disk)->download($path);
+        $file = Storage::disk($disk)->get($path);
+        return response($file, 200, ['Content-Type' => Storage::mimeType($path)]);
     })
         ->where('path', '.*')
         ->middleware(config('local-temporary-url.middleware'))
